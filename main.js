23226f704739a073e059776044131d737836ba9a
Okami-4942 <rio.ksmt@gmail.com>
2026-05-10 15:00:19 +0900

    ロビー大枠

--- main.js
+++ main.js
@@ -50,32 +50,6 @@ const plate = new THREE.Mesh(floor_geometry, floor_material);
 plate.position.y =0.009; // 厚みの半分だけ上に
 scene.add(plate);
 
-//浮島の作成
-const floor1 = new THREE.Mesh(floor_geometry, floor_material);
-floor1.position.set(30, 0.04, 10);
-scene.add(floor1);
-floor1.receiveShadow = true;
-floor1.castShadow = false;
-
-const floor2 = new THREE.Mesh(floor_geometry, floor_material);
-floor2.position.set(10, 0.04, 25);
-scene.add(floor2);
-floor2.receiveShadow = true;
-floor2.castShadow = false;
-
-const floor3 = new THREE.Mesh(floor_geometry, floor_material);
-floor3.position.set(-25, 0.04, -80);
-scene.add(floor3);
-floor3.receiveShadow = true;
-floor3.castShadow = false;
-
-const floor4 = new THREE.Mesh(floor_geometry, floor_material);
-floor4.position.set(-20, 0.04, -20);
-scene.add(floor4);
-floor4.receiveShadow = true;
-floor4.castShadow = false;
-
-
 //床に影を落す
 plate.receiveShadow = true;
 plate.castShadow = false;
@@ -103,24 +77,20 @@ camera.add(crosshair);
 // ---------- GLB の読み込み ----------
 const loader = new GLTFLoader()
 
-const glbPath1 = "./models/table.glb"
-const glbPath2 = "./models/yuutu.glb"
-const glbPath3 = "./models/puranetes.glb"
-const glbPath4 = "./models/setuna.glb" 
-const glbPath5 = "./models/table.glb"
-const glbPath6 = "./models/dec.glb"
-const glbPath7 = "./models/tuki.glb"
+const glbPath1 = "./models/fdoor.glb"
+const glbPath2 = "./models/mdoor.glb"
+const glbPath3 = "./models/pdoor.glb"
 
-//tableのオブジェクトを読み込み
+//fdoorの読み込み
   loader.load(
   glbPath1,
   function (gltf) {
     const model1 = gltf.scene; //<-ここの変数を増やす
     
     // モデルのサイズや位置を調整
-    model1.scale.set(0.7, 0.7, 0.7); //モデルの大きさを調整
-    model1.rotation.set(0, Math.PI/2, 0); // モデルの回転を調整
-    model1.position.set(0, -0.4, 0.1);//モデルの位置を調整
+    model1.scale.set(0.5, 0.5, 0.5); //モデルの大きさを調整
+    model1.rotation.set(0, Math.PI, 0); // モデルの回転を調整
+    model1.position.set(5.8,1.95,0);//モデルの位置を調整
     model1.receiveShadow = true;//影を付ける
     
         // 子要素にも影の設定を適用
@@ -135,65 +105,41 @@ const glbPath7 = "./models/tuki.glb"
     console.log("モデル1が正常に読み込まれました。");
   })
 
-  //ふたつめtableのオブジェクトを読み込み
+  //mdoorのオブジェクトを読み込み
   loader.load(
-  glbPath1,
+  glbPath2,
   function (gltf) {
-    const model5 = gltf.scene; //<-ここの変数を増やす
+    const model2 = gltf.scene; //<-ここの変数を増やす
     
     // モデルのサイズや位置を調整
-    model5.scale.set(0.7, 0.7, 0.7); //モデルの大きさを調整
-    model5.rotation.set(0,-Math.PI/2,0); // モデルの回転を調整
-    model5.position.set(0, -0.4,-0.1);//モデルの位置を調整
-    model5.receiveShadow = true;//影を付ける
+    model2.scale.set(0.5, 0.5, 0.5); //モデルの大きさを調整
+    model2.rotation.set(0, Math.PI/3, 0); // モデルの回転を調整
+    model2.position.set(-2.9,1.95,5.023);//モデルの位置を調整
+    model2.receiveShadow = true;//影を付ける
     
         // 子要素にも影の設定を適用
-    model5.traverse((child) => {
+    model2.traverse((child) => {
       if (child.isMesh) {
         child.castShadow = true;
         child.receiveShadow = true;
       }
     });
    
-    scene.add(model5);
-    console.log("モデル5が正常に読み込まれました。");
+    scene.add(model2);
+    console.log("モデル2が正常に読み込まれました。");
   })
 
-  //メアリー・スーの憂鬱のオブジェクトを読み込み
-  loader.load(
-    glbPath2,
-    function (gltf) {
-      const model2 = gltf.scene; //<-ここの変数を増やす
-    
-      // モデルのサイズや位置を調整
-      model2.scale.set(0.01, 0.01, 0.01); //モデルの大きさを調整
-      model2.rotation.set(Math.PI,0, Math.PI); // モデルの回転を調整
-      model2.position.set(0, 1.8,4.85);//モデルの位置を調整
-      model2.receiveShadow = true;//影を付ける
-    
-            // 子要素にも影の設定を適用
-      model2.traverse((child) => {
-        if (child.isMesh) {
-          child.castShadow = true;
-          child.receiveShadow = true;
-        }
-      });
-   
-      scene.add(model2);
-      console.log("モデル2が正常に読み込まれました。");
-    })
-
-     //プラネテスのオブジェクトを読み込み
+  //pdoorオブジェクトを読み込み
   loader.load(
     glbPath3,
     function (gltf) {
       const model3 = gltf.scene; //<-ここの変数を増やす
     
       // モデルのサイズや位置を調整
-      model3.scale.set(0.01, 0.01, 0.01); //モデルの大きさを調整
-      model3.rotation.set(Math.PI,Math.PI/2.8, Math.PI); // モデルの回転を調整
-      model3.position.set(-3.65,1.8,1.3);//モデルの位置を調整
-      model3.receiveShadow = true;//影を付ける
+    model3.scale.set(0.5, 0.5, 0.5); //モデルの大きさを調整
+    model3.rotation.set(0, Math.PI*-1/3, 0); // モデルの回転を調整
+    model3.position.set(-2.9,1.95,-5.023);//モデルの位置を調整
+    model3.receiveShadow = true;//影を付ける
     
             // 子要素にも影の設定を適用
       model3.traverse((child) => {
@@ -205,81 +151,7 @@ const glbPath7 = "./models/tuki.glb"
    
       scene.add(model3);
       console.log("モデル3が正常に読み込まれました。");
-    })
-
-    //陽だまりのセツナのオブジェクトを読み込み
-  loader.load(
-    glbPath4,
-    function (gltf) {
-      const model4 = gltf.scene; //<-ここの変数を増やす
-    
-      // モデルのサイズや位置を調整
-      model4.scale.set(0.01, 0.01, 0.01); //モデルの大きさを調整
-      model4.rotation.set(Math.PI,-Math.PI/2.5, Math.PI); // モデルの回転を調整
-      model4.position.set(2.7, 1.8,3.5);//モデルの位置を調整
-      model4.receiveShadow = true;//影を付ける
-    
-            // 子要素にも影の設定を適用
-      model4.traverse((child) => {
-        if (child.isMesh) {
-          child.castShadow = true;
-          child.receiveShadow = true;
-        }
-      });
-   
-      scene.add(model4);
-      console.log("モデル4が正常に読み込まれました。");
-    })
-
-     //Dec.のオブジェクトを読み込み
-  loader.load(
-    glbPath6,
-    function (gltf) {
-      const model6 = gltf.scene; //<-ここの変数を増やす
-    
-      // モデルのサイズや位置を調整
-      model6.scale.set(0.01, 0.01, 0.01); //モデルの大きさを調整
-      model6.rotation.set(Math.PI,Math.PI/1.4, Math.PI); // モデルの回転を調整
-      model6.position.set(-1.2, 1.83,-5.2);//モデルの位置を調整
-      model6.receiveShadow = true;//影を付ける
-    
-         // 子要素にも影の設定を適用
-      model6.traverse((child) => {
-        if (child.isMesh) {
-          child.castShadow = true;
-          child.receiveShadow = true;
-        }
-      });
-   
-      scene.add(model6);
-      console.log("モデル6が正常に読み込まれました。");
-    })
-
-     //ツキアカリのオブジェクトを読み込み
-  loader.load(
-    glbPath7,
-    function (gltf) {
-      const model7 = gltf.scene; //<-ここの変数を増やす
-    
-      // モデルのサイズや位置を調整
-      model7.scale.set(0.01, 0.01, 0.01); //モデルの大きさを調整
-      model7.rotation.set(Math.PI,-Math.PI/1.4, Math.PI); // モデルの回転を調整
-      model7.position.set(1.9,1.85,-4.35);//モデルの位置を調整
-      model7.receiveShadow = true;//影を付ける
-    
-           // 子要素にも影の設定を適用
-      model7.traverse((child) => {
-        if (child.isMesh) {
-          child.castShadow = true;
-          child.receiveShadow = true;
-        }
-      });
-   
-      scene.add(model7);
-      console.log("モデル7が正常に読み込まれました。");
-    })
-
-     
+    })   
 
 //光  
 
@@ -301,37 +173,6 @@ const glbPath7 = "./models/tuki.glb"
 
 const fill = new THREE.HemisphereLight(0xffffff, 0x444444, 0.5);
 scene.add(fill);
-
-  // 点光源を作成
-
-  // スポットライト光源を作成
-// new THREE.SpotLight(色, 光の強さ, 距離, 照射角, ボケ具合, 減衰率)
-const light1 = new THREE.SpotLight(0Xffe9b3, 15, 2, Math.PI, 4, 0.5);
-scene.add(light1);
-light1.position.set(0, 3, 4.7);
-light1.castShadow = false;
-
-const light2 = new THREE.SpotLight(0Xffe9b3, 15, 2, Math.PI, 4, 0.5);
-scene.add(light2);
-light2.position.set(-3.1, 3, 2.7);
-light2.castShadow = false;
-
-const light3 = new THREE.SpotLight(0Xffe9b3, 15, 2, Math.PI, 4, 0.5);
-scene.add(light3);
-light3.position.set(3.1, 3, 2.7);
-light3.castShadow = false;
-
-// light4は冊数的に削除
-
-const light5 = new THREE.SpotLight(0Xffe9b3, 15, 2, Math.PI, 4, 0.5);
-scene.add(light5);
-light5.position.set(-2.5,3,-4.1);
-light5.castShadow = false;
-
-const light6 = new THREE.SpotLight(0Xffe9b3, 15, 2, Math.PI, 4, 0.5);
-scene.add(light6);
-light6.position.set(2.5,3,-4.1);
-light6.castShadow = false;
 
 //fps設定
 
